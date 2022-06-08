1e57c3ec10aeb019c3888dd0c65ecb55dae3ebcf
nntivari11111 <102126830+nntivari11111@users.noreply.github.com>
2022-06-08 12:50:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Main from "./Components/Main";
+// import S2Day1 from "./assignments/S2Day1"
+// import S2Day3 from "./assignments/S2Day3"
+//import Navbar from"./assignments/s2d4navbar/navbar"
 
 function App() {
   return (
     <div>
       <Main />
+     {/* <S2Day1/> */}
+      {/* <S2Day3/> */}
     </div>
   );
 }
