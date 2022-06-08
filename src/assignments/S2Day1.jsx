c5bfd3d7b45f18862ca3cde59c381315814f3f8c
nntivari11111 <102126830+nntivari11111@users.noreply.github.com>
2022-06-08 12:41:55 +0530

    Add files via upload

---
+++ src/assignments/S2Day1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Todos from "./TodoWithServer/Todos";
+
+const S2Day1 = () => {
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
+};
+
+export default S2Day1;
---
+++ src/assignments/S2Day3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./ReactForms/Form";
+
+const S2Day3 = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default S2Day3;
